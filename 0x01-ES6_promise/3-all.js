342abfdeabc3b198e43dbfca8d381e79ff10f432
Valentine Maduagwu <vchidubem9@gmail.com>
2024-06-21 02:17:00 +0100

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((values) => {
-    console.log(values[0].body, values[1].firstName, values[1].lastName);
-  });
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  }).catch(() => console.error('Signup system offline'));
 }
