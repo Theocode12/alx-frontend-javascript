efb66e8662e796c99108b94857ff72382c8cc30b
Valentine Maduagwu <vchidubem9@gmail.com>
2024-06-21 01:53:00 +0100

    q2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then((value) => new Promise(value))
+  return promise.then((value) => new Promise(value))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
