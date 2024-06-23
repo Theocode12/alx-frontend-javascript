14907b67b0a0090f1de019dcab0332e3aad9c58a
Valentine Maduagwu <vchidubem9@gmail.com>
2024-06-23 16:41:26 +0100

    q100

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+
+  cloneCar() {
+    const Clone = this.constructor[Symbol.species];
+    return new Clone();
+  }
+}
