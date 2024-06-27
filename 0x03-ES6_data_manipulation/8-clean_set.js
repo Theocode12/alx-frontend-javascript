399a2206f15ab4ee307f0ae9ade650aadacbb980
Valentine Maduagwu <vchidubem9@gmail.com>
2024-06-27 23:39:10 +0100

    q8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const res = [];
 
-  if (startString && typeof startString === 'string') {
+  if (startString && typeof startString === 'string' && (set instanceof Set)) {
     set.forEach((value) => {
       if (startString && value.startsWith(startString)) {
         res.push(value.replace(startString, ''));
